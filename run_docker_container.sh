ce792654fa49534c513f6529fdae15daca1258e3
harishkumarbalaji <harishkumarbalaji@gmail.com>
2025-04-26 19:23:00 +0000

    fix docker entrypoint

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -3,6 +3,8 @@
 if [ "$(docker ps -q -f name=ros-noetic-container)" ]; then
     docker exec -it ros-noetic-container bash
 else
-    UID=$(id -u) GID=$(id -g) docker compose -f setup/docker-compose.yaml up -d
+    export HOST_PATH=${PWD#$HOME}
+    export CONTAINER_WD="/home/${USER}/host/${PWD#$HOME/}../../../"
+    UID=$(id -u) GID=$(id -g) CONTAINER_WD="$CONTAINER_WD" docker compose -f setup/docker-compose.yaml up -d
     docker exec -it ros-noetic-container bash
 fi
