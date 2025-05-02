04b5027cddf9af913229e650c8d7931d7395f451
pravshot <praveenkalva@icloud.com>
2025-05-02 17:22:42 -0500

    rename to insnavgeod

--- gem_simulator/gem_gazebo/scripts/insnavgeod_publisher.py
+++ gem_simulator/gem_gazebo/scripts/insnavgeod_publisher.py
@@ -9,7 +9,7 @@ import tf.transformations
 
 from septentrio_gnss_driver.msg import INSNavGeod
 
-class INSPublisher(object):
+class INSNavGeodPublisher(object):
 
     def __init__(self):
         self.latest_gps = None
@@ -22,7 +22,7 @@ class INSPublisher(object):
         self.gps_vel_sub = rospy.Subscriber("/gps/fix_velocity", Vector3Stamped, self.gps_vel_callback)
 
         # Publisher
-        self.ins_pub = rospy.Publisher("/insnavgeod", INSNavGeod, queue_size=10)
+        self.insnavgeod_pub = rospy.Publisher("/insnavgeod", INSNavGeod, queue_size=10)
 
 
     def gps_callback(self, msg):
@@ -33,7 +33,7 @@ class INSPublisher(object):
     def imu_callback(self, msg):
         """Sets latest IMU data"""
         self.latest_imu = msg
-    
+
     def gps_vel_callback(self, msg):
         """
         Sets latest GPS velocity data.
@@ -52,31 +52,31 @@ class INSPublisher(object):
         if not current_gps or not current_imu or not current_gps_velocity:
             return
 
-        ins_msg = INSNavGeod()
-        ins_msg.header.stamp = current_gps.header.stamp
-        ins_msg.header.frame_id = current_gps.header.frame_id
-        ins_msg.error = current_gps.status.status
+        msg = INSNavGeod()
+        msg.header.stamp = current_gps.header.stamp
+        msg.header.frame_id = current_gps.header.frame_id
+        msg.error = current_gps.status.status
 
-        ins_msg.latitude = current_gps.latitude
-        ins_msg.longitude = current_gps.longitude
-        ins_msg.height = current_gps.altitude
+        msg.latitude = current_gps.latitude
+        msg.longitude = current_gps.longitude
+        msg.height = current_gps.altitude
 
         # convert orientation to roll, pitch, yaw
         q = current_imu.orientation
         roll, pitch, yaw = tf.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])
         # convert to degrees
-        ins_msg.roll, ins_msg.pitch, ins_msg.heading = math.degrees(roll), math.degrees(pitch), math.degrees(yaw)
-        
-        ins_msg.ve, ins_msg.vn, ins_msg.vu = current_gps_velocity.vector.x, current_gps_velocity.vector.y, current_gps_velocity.vector.z
-        
-        self.ins_pub.publish(ins_msg)
+        msg.roll, msg.pitch, msg.heading = math.degrees(roll), math.degrees(pitch), math.degrees(yaw)
+
+        msg.ve, msg.vn, msg.vu = current_gps_velocity.vector.x, current_gps_velocity.vector.y, current_gps_velocity.vector.z
+
+        self.insnavgeod_pub.publish(msg)
 
 # Main execution
 if __name__ == '__main__':
     try:
-        rospy.init_node('ins_publisher_node', anonymous=True)
-        ins_publisher_object = INSPublisher()
+        rospy.init_node('insnavgeod_publisher_node', anonymous=True)
+        ins_publisher_object = INSNavGeodPublisher()
         rospy.spin()
 
     except Exception as e:
-        rospy.logerr(f"An error occurred in the INS Publisher node: {e}")
+        rospy.logerr(f"An error occurred in the INSNavGeod Publisher node: {e}")
