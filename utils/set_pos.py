2d18208d0a7a2296adfb2a12f47b77d22f1e54a7
danielzhuang11 <danielzhuang11@hotmail.com>
2025-05-14 00:33:21 -0500

    fixed utils (and turned sky blue)

--- utils/set_pos.py
+++ utils/set_pos.py
@@ -1,8 +1,4 @@
-import sys
-import os
 import argparse
-
-import numpy as np
 
 import rospy
 from gazebo_msgs.msg import ModelState
@@ -11,11 +7,15 @@ from gazebo_msgs.srv import GetModelState
 
 from util import euler_to_quaternion
 
-def getModelState():
+GEM_E2 = "gem_e2"
+GEM_E4 = "gem_e4"
+
+def getModelState(vehicle_name):
+    print(f"Getting state for vehicle: {vehicle_name}")
     rospy.wait_for_service('/gazebo/get_model_state')
     try:
         serviceResponse = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-        modelState = serviceResponse(model_name='gem_e4')
+        modelState = serviceResponse(model_name=vehicle_name)
     except rospy.ServiceException as exc:
         rospy.loginfo("Service did not process request: "+str(exc))
     return modelState
@@ -28,19 +28,18 @@ def setModelState(model_state):
     except rospy.ServiceException as e:
         rospy.loginfo("Service did not process request: "+str(e))
 
-def set_position(x = 0,y = 0, yaw=0):
-    
-    rospy.init_node("set_pos")
-
-    curr_state = getModelState()
+def set_position(x = 0,y = 0, yaw=0, vehicle_name = GEM_E4):
+    curr_state = getModelState(vehicle_name)
     new_state = ModelState()
-    new_state.model_name = 'gem_e4'
+
+    new_state.model_name = vehicle_name
     new_state.twist.linear.x = 0
     new_state.twist.linear.y = 0
     new_state.twist.linear.z = 0
     new_state.pose.position.x = x
     new_state.pose.position.y = y
-    new_state.pose.position.z = 1
+    new_state.pose.position.z = -1
+   
     q = euler_to_quaternion([0,0,yaw])
     new_state.pose.orientation.x = q[0]
     new_state.pose.orientation.y = q[1]
@@ -52,25 +51,24 @@ def set_position(x = 0,y = 0, yaw=0):
     setModelState(new_state)
 
 if __name__ == "__main__":
+    # Initialize the ROS node first, before accessing parameters
+    rospy.init_node("set_pos")
+    
     parser = argparse.ArgumentParser(description = 'Set the x, y position of the vehicle')
-
-    x_default = 0
-    y_default = -98
-    yaw_default = 0
-
-    # x_default = 160
-    # y_default = 96.5
-    # yaw_default = 3.14
-
+    x_default = 17.81
+    y_default = -10
+    yaw_default = 3.14
 
     parser.add_argument('--x', type = float, help = 'x position of the vehicle.', default = x_default)
     parser.add_argument('--y', type = float, help = 'y position of the vehicle.', default = y_default)
     parser.add_argument('--yaw', type = float, help = 'yaw of the vehicle.', default = yaw_default)
+    parser.add_argument('--vehicle', type = str, help = 'vehicle name (gem_e4 or gem_e2)', default = GEM_E4)
 
     argv = parser.parse_args()
 
     x = argv.x
     y = argv.y
     yaw = argv.yaw
+    vehicle_name = argv.vehicle
 
-    set_position(x = x, y = y, yaw = yaw)
+    set_position(x = x, y = y, yaw = yaw, vehicle_name = vehicle_name)
